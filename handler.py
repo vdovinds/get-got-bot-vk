8e8c8c6edb184e2100668ff25691b7c94d56a9dc
Dmitry Vdovin <dmitry.vdovin@raiffeisen.ru>
2023-03-16 23:07:54 +0300

    Init

--- handler.py
+++ handler.py
@@ -1,6 +1,21 @@
+import json
+import os
+
+confirmation_code = os.environ.get('VK_CONFIRMATION_CODE')
+
+
 def handler(event, _):
-    print(event)
-    return {
-        'statusCode': 200,
-        'body': '!',
-    }
+    data = json.loads(event['body'])
+    print(data)
+
+    if data['type'] == 'confirmation':
+        return {
+            'statusCode': 200,
+            'body': confirmation_code,
+        }
+    else:
+        print("Новое сообщение")
+        return {
+            'statusCode': 200,
+            'body': '!',
+        }
